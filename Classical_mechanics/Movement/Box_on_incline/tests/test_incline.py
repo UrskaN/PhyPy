42d851ad02bb6fd1882554e86dd931cd09e12276
Marcel Salmič <salmic.marcel@gmail.com>
2015-02-20 21:11:37 +0100

    Writen first tests for initalizing Box_on_incline

--- Classical_mechanics/Movement/Box_on_incline/tests/test_incline.py
+++ Classical_mechanics/Movement/Box_on_incline/tests/test_incline.py
@@ -1,1 +1,41 @@
+import unittest
 import sys
+import os
+sys.path.append(os.path.dirname(os.path.realpath(__file__))+'/../')
+#uncomment the folowing line  once file is created
+#from box_on_incline import Box_on_incline as Incline
+
+#Box_on_incline(angle,m,k=0,g=9,81)
+#arguments :  angle,coefficient,grafity accleration
+
+
+class box_on_incline_test(unittest.TestCase):
+    def test_incline_create_object_returns_error_message_if_no_arguments(self):
+        #We try to create obejct without arguments (neded arguments are: angle, m)
+        self.assertRaises(TypeError,Incline)
+    def test_incline_create_object_returns_error_message_if_angle_not_given(self):
+        #We try to create obejct without arguments (neded arguments are: angle, m)
+        self.assertRaises(TypeError,Incline,m=5)
+
+    def test_incline_create_object_returns_error_message_if_mass_not_given(self):
+        #We try to create obejct without arguments (neded arguments are: angle, m)
+        self.assertRaises(TypeError,Incline,2)
+
+    def test_incline_create_object_returns_error_message_if_mass_not_number(self):
+        #We try to create obejct without arguments (neded arguments are: angle, m)
+        self.assertRaises(TypeError,Incline,m='mass')
+
+    def test_incline_create_object_returns_error_message_if_angle_not_number(self):
+        #We try to create obejct without arguments (neded arguments are: angle, m)
+        self.assertRaises(TypeError,Incline,'angle')
+        self.assertRaises(TypeError,Incline,None)
+
+
+
+
+
+
+
+
+if __name__== '__main__':
+    unittest.main()
