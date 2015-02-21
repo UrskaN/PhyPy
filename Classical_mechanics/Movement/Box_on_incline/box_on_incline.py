84b2f18c49d626f9ec58f0735fd6c113079b4403
Marcel Salmič <salmic.marcel@gmail.com>
2015-02-21 23:25:30 +0100

    Created methods for addind data to box on incline, 2 tests are a pass

--- Classical_mechanics/Movement/Box_on_incline/box_on_incline.py
+++ Classical_mechanics/Movement/Box_on_incline/box_on_incline.py
@@ -1,4 +1,34 @@
 import math
 
 class Box_on_incline(object):
-	pass
+	def __init__(self,**kwargs):
+		self.angle=None
+		self.width=None
+		self.height=None
+		
+		self.m=None
+		self.Fg=None
+		self.Fd=None
+		
+		self.ktr=None
+		self.Ftr=None
+
+		self.add_data(**kwargs)
+
+	def add_data(self,**kwargs):
+		self.angle=kwargs['angle'] if 'angle' in kwargs else self.angle
+		self.width=kwargs['width'] if 'width' in kwargs else self.width
+		self.height=kwargs['height'] if 'height' in kwargs else self.height
+
+		self.m=kwargs['m'] if 'm' in kwargs else self.m
+		self.Fg=kwargs['Fg'] if 'Fg' in kwargs else self.Fg
+		self.Fd=kwargs['Fd'] if 'Fd' in kwargs else self.Fd
+
+		self.ktr=kwargs['ktr'] if 'ktr' in kwargs else self.ktr
+		self.Ftr=kwargs['Ftr'] if 'Ftr' in kwargs else self.Ftr
+
+
+	def delete_data(self,args):
+		pass
+	def calculate(self,args):
+		pass
--- Classical_mechanics/Movement/Box_on_incline/tests/test_incline.py
+++ Classical_mechanics/Movement/Box_on_incline/tests/test_incline.py
@@ -3,7 +3,7 @@ import sys
 import os
 import math
 sys.path.append(os.path.dirname(os.path.realpath(__file__))+'/../')
-#uncomment the folowing line  once file is created
+
 from box_on_incline import Box_on_incline
 
 #Box_on_incline(**kwargs):  -->  (angle,height,width)  ,(mass,Fg,Fd,Fs) , (Ftr,ktr)
@@ -23,30 +23,30 @@ class box_on_incline_test(unittest.TestCase):
                 self.incline=Box_on_incline()
         def test_incline_add_data(self):
     		#We try to add data to object
-                self.incline.add(m=1,angle=30)
+                self.incline.add_data(m=1,angle=30)
                 self.assertEqual(1,self.incline.m)
                 self.assertEqual(30,self.incline.angle)
         def test_incline_overrides_old_data_with_new(self):
-                self.incline.add(angle=25)
+                self.incline.add_data(angle=25)
                 self.assertEqual(25,self.incline.angle)
         def test_incline_add_returns_error_on_wrong_angle(self):
-        	self.assertRaises(ValueError,self.incline.add,angle=110)
+        	self.assertRaises(ValueError,self.incline.add_data,angle=110)
         def test_incline_add_returns_error_on_to_big_ktr(self):
-                self.assertRaises(ValueError,self.incline.add,ktr=1.2)
+                self.assertRaises(ValueError,self.incline.add_data,ktr=1.2)
         def test_incline_add_returns_error_on_negative_ktr(self):
-                self.assertRaises(ValueError,self.incline.add,ktr=-0.2)
+                self.assertRaises(ValueError,self.incline.add_data,ktr=-0.2)
         def test_incline_delete_deletes_all_data_when_called_without_args(self):
-                self.incline.data()
+                self.incline.delete_data()
                 self.assertEqual(None,self.incline.m)
                 self.assertEqual(None,self.incline.angle)
                 #TODO check that all of variables are none
         def test_incline_calculate_returns_error_for_opossing_arguments(self):
-                self.incline.add(m=1,angle=30,height=10,width=1)#angle should be around 84
+                self.incline.add_data(m=1,angle=30,height=10,width=1)#angle should be around 84
                 self.assertRaises(ValueError,self.incline.calculate)
 		#TODO check for other cases
         def test_incline_calculate_returns_right_value_for_forces(self):
-                self.incline.delete()
-                self.incline.add(m=1,angle=30)
+                self.incline.delete_data()
+                self.incline.add_data(m=1,angle=30)
                 self.incline.calculate()
                 self.assertEqual(9.81,self.incline.fg)
                 self.assertEqual(9.81/2,fd)
