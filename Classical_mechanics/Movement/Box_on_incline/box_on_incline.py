9e607f3c1cd911fb007e2262884155f412ef3010
Marcel Salmič <salmic.marcel@gmail.com>
2015-02-22 18:18:42 +0100

    Recatored some of the code, tried to make variables as easyt accses from loops as possible

--- Classical_mechanics/Movement/Box_on_incline/box_on_incline.py
+++ Classical_mechanics/Movement/Box_on_incline/box_on_incline.py
@@ -1,47 +1,30 @@
 import math
 
 class Box_on_incline(object):
-	def __init__(self,**kwargs):
-		self.angle=None
-		self.width=None
-		self.height=None
-		
-		self.m=None
-		self.Fg=None
-		self.Fd=None
-		
-		self.kf=None
-		self.Ff=None
+        def __init__(self,**kwargs):
+                self.attr=['angle','width','height','m','Fg','Fs','Fd','kf','Ff']
+                 # attributes of the incline in order: angle,width,height, mass,Fg(gravity force),Fs(statical force), Fd (dynamical force),kf(friction coefficient), Ff(friction force)
+                self.data=dict()
+                for param in self.attr:
+                        self.data[param]=None
+                self.given_data=set() #set of data given by user
+                self.add_data(**kwargs)
 
-		self.given_data=set()
-		self.add_data(**kwargs)
+        def add_data(self,**kwargs):
+                self.given_data.update(kwargs.keys())
+                for i in range(len(self.attr)):
+                        param=self.attr[i]
+                        if param in kwargs:
+                                if i==0 and not (0 <= kwargs['angle'] <= 90) :# atribute is angle
+                                        raise ValueError('Angle should be between 0 an 90 degrees')
+                                elif i==7 and not (0 <= kwargs[param] <= 1):
+                                        raise ValueError('Coefficient (kf) should be between 0 and 1')
+                                else:
+                                        self.data[param]=kwargs[param]
 
-	def add_data(self,**kwargs):
-		self.given_data.update(kwargs.keys())
-		if 'angle' in kwargs:
-			if 0 <= kwargs['angle'] <= 90 :
-				self.angle=kwargs['angle']
-			else:
-				raise ValueError('Angle should be between 0 an 90 degrees')
-		self.width=kwargs['width'] if 'width' in kwargs else self.width
-		self.height=kwargs['height'] if 'height' in kwargs else self.height
-
-		self.m=kwargs['m'] if 'm' in kwargs else self.m
-		self.Fg=kwargs['Fg'] if 'Fg' in kwargs else self.Fg
-		self.Fd=kwargs['Fd'] if 'Fd' in kwargs else self.Fd
-		
-		if 'kf' in kwargs:
-			if 0 <= kwargs['kf'] <= 1:
-				self.kf=kwargs['kf']
-			else:
-				raise ValueError('Coefficient (kf) should be between 0 and 1')
-		self.Ff=kwargs['Ff'] if 'Ff' in kwargs else self.Ff
+        def delete_data(self,*args):
+                self.__init__()
+        def calculate(self,*args):
+                pass
 
 
-	def delete_data(self,*args):
-		pass
-	def calculate(self,*args):
-		pass
-	
-	def check_data(self,*args):
-		pass
--- Classical_mechanics/Movement/Box_on_incline/tests/test_incline.py
+++ Classical_mechanics/Movement/Box_on_incline/tests/test_incline.py
@@ -21,14 +21,15 @@ from box_on_incline import Box_on_incline
 class box_on_incline_test(unittest.TestCase):
         def setUp(self):
                 self.incline=Box_on_incline()
+                self.attr=self.incline.attr
         def test_incline_add_data(self):
     		#We try to add data to object
                 self.incline.add_data(m=1,angle=30)
-                self.assertEqual(1,self.incline.m)
-                self.assertEqual(30,self.incline.angle)
+                self.assertEqual(1,self.incline.data['m'])
+                self.assertEqual(30,self.incline.data['angle'])
         def test_incline_overrides_old_data_with_new(self):
                 self.incline.add_data(angle=25)
-                self.assertEqual(25,self.incline.angle)
+                self.assertEqual(25,self.incline.data['angle'])
         def test_incline_add_returns_error_on_wrong_angle(self):
         	self.assertRaises(ValueError,self.incline.add_data,angle=110)
         def test_incline_add_returns_error_on_to_big_kf(self):
@@ -38,14 +39,9 @@ class box_on_incline_test(unittest.TestCase):
         def test_incline_delete_deletes_all_data_when_called_without_args(self):
                 self.incline.add_data(m=1,angle=4,height=4,Fg=9.81,width=4,kf=0,Ff=3)
                 self.incline.delete_data()
-                self.assertEqual(None,self.incline.m)
-                self.assertEqual(None,self.incline.Fg)
-                self.assertEqual(None,self.incline.Fd)
-                self.assertEqual(None,self.incline.height)
-                self.assertEqual(None,self.incline.angle)
-                self.assertEqual(None,self.incline.width)
-                self.assertEqual(None,self.incline.kf)
-                self.assertEqual(None,self.incline.Ff)
+                for param in self.attr:
+	                self.assertEqual(None,self.incline.data[param])
+
         def test_incline_calculate_returns_error_for_opossing_arguments(self):
                 self.incline.add_data(m=1,angle=30,height=10,width=1)#angle should be around 84
                 self.assertRaises(ValueError,self.incline.calculate)
@@ -54,9 +50,9 @@ class box_on_incline_test(unittest.TestCase):
                 self.incline.delete_data()
                 self.incline.add_data(m=1,angle=30)
                 self.incline.calculate()
-                self.assertEqual(9.81,self.incline.Fg)
-                self.assertEqual(9.81/2,Fd)
-                self.assertEqual(9.81*math.sqrt(3)/2,self.incline.Fs)
+                self.assertEqual(9.81,self.incline.data['Fg'])
+                self.assertEqual(9.81/2,self.incline.data['Fd'])
+                self.assertEqual(9.81*math.sqrt(3)/2,self.incline.data['Fs'])
 
 
 
